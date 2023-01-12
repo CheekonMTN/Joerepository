ca7715bcc0e55667c6166f1edd1b32465ad88968
CheekonMTN <112905139+CheekonMTN@users.noreply.github.com>
2023-01-11 21:50:26 -0500

    Add files via upload

--- mikeshangmangame.py
+++ mikeshangmangame.py
@@ -8,17 +8,17 @@ import string
 def get_new_word() -> str:
     with open("usa.txt") as file:
         file_raw: str = file.read()
-        dictionary = file_raw.split("\n")
-        length_of_file = len(dictionary)
-        line_selection = random.randint(0, length_of_file-1)
-        word_selection = dictionary[line_selection]
+        dictionary: list = file_raw.split("\n")
+        length_of_file: int = len(dictionary)
+        line_selection: int = random.randint(0, length_of_file-1)
+        word_selection: str = dictionary[line_selection]
 
         return word_selection
 
 # Function for displaying current progress on the word
 
 def gen_word_output(answer: str, guesses: list[str]) -> str:
-    output = ""
+    output: str = ""
     for letter in answer:
         if letter in guesses:
             output = output + letter
@@ -48,50 +48,60 @@ def gen_output(answer: str, lives: int, guesses: list[str]) -> str:
     
 def gameplay(word):
 
-    number_of_lives: int = 5
+    number_of_lives: int = 10
     game_won: str = False
     guessed_letters: list = []
     alphabet: list = list(string.ascii_lowercase)
-    selected_word_for_game: str = get_new_word()
-    current_progress: str = gen_output(selected_word_for_game, number_of_lives, guessed_letters)
+    selected_word_for_game: str = word
+    
     print("Let's play some hangman!")
     print(gen_word_output(selected_word_for_game, guessed_letters))
-    print("This is how many lives you start with. " + str(number_of_lives))
+    print("This is how many lives you start with - " + str(number_of_lives))
     print("\n")
+    
     while not game_won and number_of_lives > 0:
+        
         user_guess: str = input("Please type your guess: ")
-
+        if "-" not in gen_word_output(selected_word_for_game, guessed_letters):
+            current_progress: str = gen_output(selected_word_for_game, number_of_lives, guessed_letters)
+            game_won = True
+            break
         #  Checking if the user's guess was correct, already guessed, or incorrect. If a word or multiple letters is input an error occurs
         if len(user_guess) != 1 or user_guess not in alphabet:
             print("not a valid entry. Please try again.")
+            current_progress: str = gen_output(selected_word_for_game, number_of_lives, guessed_letters)
             print(current_progress)
             continue
             # if user_guess was used already
         if user_guess in guessed_letters:
             print("You have already guessed that letter! Try again.")
+            current_progress: str = gen_output(selected_word_for_game, number_of_lives, guessed_letters)
             print(current_progress)
             continue
         if user_guess not in selected_word_for_game:
             print(user_guess, "is not in the word. Try again!")
             number_of_lives -= 1
             guessed_letters.append(user_guess)
+            current_progress: str = gen_output(selected_word_for_game, number_of_lives, guessed_letters)
             print(current_progress)
             continue
+            
         # if user_guess is in the word, guessed_letters list is updated
         else: 
             print(user_guess, "is in the word!")
             guessed_letters.append(user_guess)
+            current_progress: str = gen_output(selected_word_for_game, number_of_lives, guessed_letters)
             print(current_progress)
             continue
-        if "-" not in gen_word_output(answer, guesses):
-            game_won = True
+    
+    
         
     # Game won or lost
-    if game_won:
-        print("Good job, you won! The word was " + word)
+    if game_won == True:
+        print("Good job, you won! The word was " + selected_word_for_game)
         
     else:
-        print("You're out of lives! You're DEAD! The correct word was " + word)
+        print("You're out of lives! You're DEAD! The correct word was " + selected_word_for_game)
 
 # Function that runs the game
 def main():
